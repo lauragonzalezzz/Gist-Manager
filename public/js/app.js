69f795bbc761a11804b79ee4f3323f5849bc2ea6
Laura <lg3295@gmail.com>
2016-05-11 20:07:37 -1000

    WIP makes ajax request to github for public gists

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,49 @@
 'use strict'
 
-var gists = [
-  {id: 1, description: "description here!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
-  {id: 2, description: "second gist!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
-  {id: 3, description: "third!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
-  {id: 4, description: "last gist!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"}
-]
+// var gists = [
+//   {id: 1, description: "description here!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
+//   {id: 2, description: "second gist!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
+//   {id: 3, description: "third!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"},
+//   {id: 4, description: "last gist!", url: "www.1234.com", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae nihil, assumenda expedita. Quod excepturi itaque numquam nesciunt, deleniti tempore. Eaque est, soluta iusto dolor doloribus alias labore reprehenderit velit odit?"}
+// ]
 
 const GistList = React.createClass({
+    getInitialState: function(){
+    return {
+      GistArr: []
+    }
+  },
+  loadDataFromGitHub: function(){
+    $.ajax({
+      url: this.props.publicGistUrl,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        console.log('data',data);
 
+
+
+
+
+        this.setState({gistData: data})
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.err(this.props.publicGistUrl, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function(){
+    this.loadDataFromGitHub()
+  },
   render: function(){
-    var gistListNode = gists.map(function(gistData) {
+    var gistListNode = this.props.gistData.map(function(gistItem){
+    console.log(gistItem);
       return (
-        <Gist
-          key={gistData.id}
-          url={gistData.url}
-          desc={gistData.description}
-          content={gistData.content}>
-        </Gist>
+        <RedditItem
+          key={gistItem.id}
+          desc={gistItem.description}
+          url={gistItem.url}>
+        </RedditItem>
       )
     })
     return (
@@ -25,7 +51,7 @@ const GistList = React.createClass({
         <h1>Gist Manager</h1>
         <h2>Gist List</h2>
         <p>By Laura</p>
-        {gistListNode}
+        <Gist gistData={this.state.gistData} />
       </div>
     )
   }
@@ -35,9 +61,7 @@ const Gist = React.createClass({
   render: function(){
     return (
       <div className="gistItem">
-        <h4>{this.props.desc}</h4>
-        <p>{this.props.url}</p>
-        <p>{this.props.content}</p>
+        <h4>HELLO</h4>
       </div>
     )
   }
@@ -45,6 +69,6 @@ const Gist = React.createClass({
 
 
 ReactDOM.render(
-  <GistList gists={gists}/>,
+  <GistList publicGistUrl="https://api.github.com/gists/public"/>,
   document.getElementById('content')
 );
