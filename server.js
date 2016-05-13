db4fcb1865fa4b73a5bb287920e312751a0e9b9a
Laura <lg3295@gmail.com>
2016-05-12 19:46:56 -1000

    adds route to go to github for authorization

--- server.js
+++ server.js
@@ -1,16 +1,58 @@
 'use strict';
 
-const express = require('express'),
-       app = express(),
-   bodyParser = require('body-parser'),
-     passport = require('passport'),
-      // session = require('express-session'),
-OAuthStrategy = require('passport-oauth').OAuthStrategy;
+const   express = require('express'),
+            app = express(),
+     bodyParser = require('body-parser'),
+       passport = require('passport'),
+        session = require('express-session'),
+ GitHubStrategy = require('passport-github2').Strategy,
+         CONFIG = require('./config/config.json');
 
+//PASSPORT
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  return done(null, obj)
+});
+passport.use(new GitHubStrategy({
+  clientID: CONFIG.ClientId,
+  clientSecret: CONFIG.ClientSecret,
+  callbackURL: "http://127.0.0.1:4321/return"
+},
+  (accessToken, refreshToken, profile, done) => {
+    profile.accessToken = accessToken;
+    return done(null, profile)
+  }
+));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//SERVER
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.secretSecret ||'keyboard cat',
+  resave: false,
+  saveUninitialized: false }));
 
-app.get()
+//ROUTES
+
+app.get('/return',
+  passport.authenticate('github',
+  {failureRedirect: "/return"})
+  ,((req, res) => {
+    // localStorage.GistAuthToken = req.user.accessToken;
+    // console.log(localStorage.GistAuthToken);
+    // res.send(req.user.accessToken)
+    res.redirect('/#' + req.user.accessToken);
+  })
+);
+
+// app.get('*', (req, res) => {
+//   passport.authenticate('github', {scope : gist})
+// });
 
 app.listen(4321, () => {
   console.log('Server is listening on port 4321');
